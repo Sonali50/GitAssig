b6fb4673e529c1bedcd8da73ebd39c1da1b46d3b
Sonali50 <cloud_user@mylabserver.com>
2020-06-22 14:27:55 +0530

    adding sub and div fun

--- src/calculator.c
+++ src/calculator.c
@@ -3,7 +3,18 @@ void calculator()
 {
 	printf("I am a empty calculator");
 }
+int addition(int a, int b)
+{
+	return a+b;
+}
+int multiplication(int a,int b)
+{
+	return a*b;
+}
 int main()
 {
 	calculator();
+	addition(x,y);
+	multiplication(x,y);
 }
+
